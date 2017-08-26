6543eea35bec12fc0b7e08944fb9668f234746dd
cdc895642 <cdc895642@hotmail.com>
2017-08-27 00:03:24 +0300

    start test ContactControllerTest

--- src/test/java/com/rest/regexp/controller/ContactControllerTest.java
+++ src/test/java/com/rest/regexp/controller/ContactControllerTest.java
@@ -6,16 +6,16 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.rest.regexp.TestData;
 import com.rest.regexp.dto.ContactList;
-import com.rest.regexp.model.Contact;
 import com.rest.regexp.service.ContactService;
-import java.util.List;
-import java.util.Set;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,43 +24,47 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ContactController.class)
 public class ContactControllerTest {
 
   @MockBean
-  private ContactService groupService;
+  private ContactService contactService;
 
   @Autowired
   private MockMvc mockMvc;
 
-  private List<Contact>  contacts;
-
   @Test
-  public void getGroupsFromUserLocation_registeredUserLocationWithGroups_returnGroups()
+  public void getContactsByNameRegexp_correctUrlAndRequestParamAndMethod_returnContacts()
       throws Exception {
     //Arrange
     final String FIRST_NAME = "Vanya";
     final String SECOND_NAME = "Petya";
-    final int RESULT_SET_SIZE = 2;
-    final String TESTED_URL = "/groups/mylocation";
+    final int RESULT_CONTACTS_SIZE = 2;
+    final String TESTED_URL = "/hello/contacts?nameFilter=filter";
     final int EXPECTED_NUMBER_OF_INVOCATION = 1;
     ContactList contactList = TestData.getContactList(FIRST_NAME, SECOND_NAME);
-    given(groupService.getContactsByNotMatchNamePattern(anyString()))
+    given(contactService.getContactsByNotMatchNamePattern(anyString()))
         .willReturn(contactList);
-//
-//    //Act && Assert
-//    mockMvc.perform(get(TESTED_URL)
-//        .contentType(MediaType.APPLICATION_JSON))
-//        .andExpect(status().isOk())
-//        .andExpect(jsonPath("$", hasSize(RESULT_SET_SIZE)))
-//        .andExpect(jsonPath("$[0].name").value(FIRST_GROUP))
-//        .andExpect(jsonPath("$[1].name").value(SECOND_GROUP));
-//
-//    //Assert
-//    verify(groupService, times(EXPECTED_NUMBER_OF_INVOCATION))
-//        .getGroupResourcesFromUserLocation(anyString());
-//    verifyNoMoreInteractions(groupService);
+
+    //Act && Assert
+    MvcResult mvcResult = mockMvc.perform(get(TESTED_URL)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(request().asyncStarted())
+        .andReturn();
+
+    mockMvc.perform(asyncDispatch(mvcResult))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+        .andExpect(jsonPath("$.contacts", hasSize(RESULT_CONTACTS_SIZE)))
+        .andExpect(jsonPath("$.contacts[0].name").value(FIRST_NAME))
+        .andExpect(jsonPath("$.contacts[1].name").value(SECOND_NAME));
+
+    //Assert
+    verify(contactService, times(EXPECTED_NUMBER_OF_INVOCATION))
+        .getContactsByNotMatchNamePattern(anyString());
+    verifyNoMoreInteractions(contactService);
   }
 }
